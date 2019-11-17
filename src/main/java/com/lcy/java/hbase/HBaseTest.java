2783e44f3227779c60634e625cabe203c720b5d7
liuchaoyangliu <38664071@qq.com>
2019-11-17 09:47:27 +0800

    2019.10.17

--- src/main/java/com/lcy/java/hbase/HBaseTest.java
+++ src/main/java/com/lcy/java/hbase/HBaseTest.java
@@ -3,27 +3,25 @@ package com.lcy.java.hbase;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class HBaseTest {
     
-    private static Connection connection;
-    private static Table table;
-    private static HBaseAdmin admin;
+    private static Connection connection = null;
+    private static HBaseAdmin admin = null;
     
     static {
-        //获取配置信息
+        //设置配置文件信息
         Configuration conf = HBaseConfiguration.create();
-        conf.set("hbase.zookeeper.quorum", "192.166.9.102");
+        conf.set("hbase.zookeeper.quorum", "");
         conf.set("hbase.zookeeper.property.clientPort", "2181");
+        
         try {
             //获取连接
             connection = ConnectionFactory.createConnection(conf);
-            //获取admin对象
+            //获取Admin对象
             admin = (HBaseAdmin) connection.getAdmin();
         } catch (IOException e) {
             e.printStackTrace();
@@ -37,127 +35,143 @@ public class HBaseTest {
     
     //创建表
     public static void createTable(String tableName, String... columnFamily) throws IOException {
-
-        //判断表是否存在
         if (isTableExist(tableName)) {
-            System.out.println("表" + tableName + "已存在");
-            //System.exit(0);
+            System.out.println("表：" + tableName + " 已经存在");
         } else {
-            //创建表属性对象,表名需要转字节
             HTableDescriptor descriptor = new HTableDescriptor(TableName.valueOf(tableName));
-            //创建多个列族
             for (String cf : columnFamily) {
                 descriptor.addFamily(new HColumnDescriptor(cf));
             }
-            //根据对表的配置，创建表
             admin.createTable(descriptor);
-            System.out.println("表" + tableName + "创建成功！");
+            System.out.println("表：" + tableName + " 已经存在");
         }
     }
     
     //删除表
-    public static void dropTable(String tableName) throws IOException {
+    public static void deleteTable(String tableName) throws IOException {
         if (isTableExist(tableName)) {
             admin.disableTable(tableName);
             admin.deleteTable(tableName);
-            System.out.println("表" + tableName + "删除成功！");
         } else {
-            System.out.println("表" + tableName + "不存在！");
+            System.out.println("表： " + tableName + "表不存在");
         }
     }
     
-    //向表中插入数据
+    //向表中插入一条数据
     public static void addRowData(String tableName,
                                   String rowKey,
                                   String columnFamily,
                                   String column,
                                   String value) throws IOException {
-        //创建HTable对象
-        table = connection.getTable(TableName.valueOf(tableName));
-        //向表中插入数据
-        Put put = new Put(Bytes.toBytes(rowKey));
-        //向Put对象中组装数据
-        put.addColumn(columnFamily.getBytes(), column.getBytes(), value.getBytes());
-        table.put(put);
-        table.close();
-        System.out.println("插入数据成功");
-    }
-    
-    //删除多行数据
-    public static void deleteMultiRow(String tableName, String... rows) throws IOException {
-        //创建HTable对象
-        table = connection.getTable(TableName.valueOf(tableName));
-        List<Delete> deleteList = new ArrayList<>();
-        for (String row : rows) {
-            Delete delete = new Delete(Bytes.toBytes(row));
-            deleteList.add(delete);
+        if (isTableExist(tableName)) {
+            //创建table对象
+            Table table = connection.getTable(TableName.valueOf(tableName));
+            //封装数据
+            Put put = new Put(rowKey.getBytes());
+            put.addColumn(columnFamily.getBytes(), column.getBytes(), value.getBytes());
+            //向表中插入数据
+            table.put(put);
+            //关闭连接
+            table.close();
+            System.out.println("插入数据成功！");
+            
+        } else {
+            System.out.println("表：" + tableName + " 不存在");
         }
-        table.delete(deleteList);
-        table.close();
     }
     
-    //获取所有数据
-    public static void getAllRows(String tableName) throws IOException {
-        table = connection.getTable(TableName.valueOf(tableName));
-        //得到用于扫描region的对象
-        Scan scan = new Scan();
-        //使用HTable得到resultScanner实现类的对象
-        ResultScanner resultScanner = table.getScanner(scan);
-        
-        for (Result result : resultScanner) {
-            Cell[] cells = result.rawCells();
-            for (Cell cell : cells) {
-                System.out.println("	行	键	:" + Bytes.toString(CellUtil.cloneRow(cell)));
-                System.out.println("	列	族	" + Bytes.toString(CellUtil.cloneFamily(cell)));
-                System.out.println("	列	:" + Bytes.toString(CellUtil.cloneQualifier(cell)));
-                System.out.println("	值	:" + Bytes.toString(CellUtil.cloneValue(cell)));
+    //删除数据 一行或多行
+    public static void deleteMultiRow(String tableName, String... rows) throws IOException {
+        if (isTableExist(tableName)) {
+            //获取Table对象
+            Table table = connection.getTable(TableName.valueOf(tableName));
+            ArrayList<Delete> deleteList = new ArrayList<>();
+            for (String row :
+                    rows) {
+                Delete delete = new Delete(row.getBytes());
+                deleteList.add(delete);
             }
+            //删除数据
+            table.delete(deleteList);
+            table.close();
+            System.out.println("删除" + tableName + "中的数据成功");
+        } else {
+            System.out.println("表：" + tableName + "不存在！");
         }
     }
     
     //获取某一行数据
     public static void getRow(String tableName, String rowKey) throws IOException {
-        table = connection.getTable(TableName.valueOf(tableName));
-        Get get = new Get(Bytes.toBytes(rowKey));
-        //get.setMaxVersions();显示所有版本
-        //get.setTimeStamp();显示指定时间戳的版本
-        Result result = table.get(get);
-        for (Cell cell : result.rawCells()) {
-            System.out.println("	行	键	:" + result.getRow().toString());
-            System.out.println("	列	族	" + CellUtil.cloneFamily(cell).toString());
-            System.out.println("	列	:" + CellUtil.cloneQualifier(cell).toString());
-            System.out.println("	值	:" + CellUtil.cloneValue(cell).toString());
-            System.out.println("    时间戳:" + cell.getTimestamp());
+        if (isTableExist(tableName)) {
+            Table table = connection.getTable(TableName.valueOf(tableName));
+            Get get = new Get(rowKey.getBytes());
+            
+            Result result = table.get(get);
+            for (Cell cell : result.rawCells()) {
+                System.out.println("行键：" + result.getRow().toString());
+                System.out.println("列族：" + CellUtil.cloneFamily(cell).toString());
+                System.out.println("列：" + CellUtil.cloneQualifier(cell).toString());
+                System.out.println("值：" + CellUtil.cloneValue(cell).toString());
+                System.out.println("时间戳：" + cell.getTimestamp());
+            }
+        } else {
+            System.out.println("表：" + tableName + " 不存在！");
         }
     }
     
-    //获取某一行指定“列族:列”的数据
+    //获取一张表所有数据
+    public static void getAllrRow(String tableName) throws IOException {
+        if (isTableExist(tableName)) {
+            Table table = connection.getTable(TableName.valueOf(tableName));
+            
+            Scan scan = new Scan();
+            ResultScanner resultScanner = table.getScanner(scan);
+            for (Result result : resultScanner) {
+                Cell[] cells = result.rawCells();
+                for (Cell cell : cells) {
+                    System.out.println("行键：" + CellUtil.cloneRow(cell).toString());
+                    System.out.println("列族：" + CellUtil.cloneFamily(cell).toString());
+                    System.out.println("列：" + CellUtil.cloneQualifier(cell).toString());
+                    System.out.println("值：" + CellUtil.cloneValue(cell).toString());
+                }
+            }
+        } else {
+            System.out.println("表：" + tableName + "不存在！");
+        }
+    }
+    
+    //获取指定“列族：列”的数据
     public static void getRowQualifier(String tableName,
                                        String rowKey,
                                        String family,
                                        String qualifier) throws IOException {
-        table = connection.getTable(TableName.valueOf(tableName));
-        Get get = new Get(rowKey.getBytes());
-        get.addColumn(family.getBytes(), qualifier.getBytes());
-        
-        Result result = table.get(get);
-        for (Cell cell : result.rawCells()) {
-            System.out.println("	行	键	:" + result.getRow().toString());
-            System.out.println("	列	族	" + CellUtil.cloneFamily(cell).toString());
-            System.out.println("	列	:" + CellUtil.cloneQualifier(cell).toString());
-            System.out.println("	值	:" + CellUtil.cloneValue(cell).toString());
+        if (isTableExist(tableName)) {
+            Table table = connection.getTable(TableName.valueOf(tableName));
+            Get get = new Get(rowKey.getBytes());
+            
+            get.addColumn(family.getBytes(), qualifier.getBytes());
+            
+            Result result = table.get(get);
+            for (Cell cell : result.rawCells()) {
+                System.out.println("行键：" + CellUtil.cloneRow(cell).toString());
+                System.out.println("列族：" + CellUtil.cloneFamily(cell).toString());
+                System.out.println("列：" + CellUtil.cloneQualifier(cell).toString());
+                System.out.println("值：" + CellUtil.cloneValue(cell).toString());
+            }
+        } else {
+            System.out.println("表：" + tableName + "不存在");
         }
     }
     
     public static void close() {
-        if(admin != null){
+        if (admin != null) {
             try {
                 admin.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        if(connection != null){
+        if (connection != null) {
             try {
                 connection.close();
             } catch (IOException e) {
